e7a11b8ab9871c7403092011dc6b5c83904cfb54
zliphadi01-ui <zliphadi01@gmail.com>
2025-11-27 08:48:50 +0700

    coba perbaiki login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,13 +13,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(PasienSeeder::class);
+        $this->call([
+            UserSeeder::class,
+            PasienSeeder::class,
+        ]);
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
     
 }
