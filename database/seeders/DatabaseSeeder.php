9f21da73301315d2944449d61e1dc40ed1ad5637
muhammadnaufalmustafa-cpu <muhammadnaufalmustafa@gmail.com>
2025-11-27 08:41:07 +0700

    memperbaiki sistem login, memperbaiki struktur database, dan mendaftarkan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(PasienSeeder::class);
+        $this->call(UserSeeder::class);
         // User::factory(10)->create();
 
         User::factory()->create([
