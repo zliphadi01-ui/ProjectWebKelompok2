ed3de511918be5e377f23d02083d69a8791f6113
zliphadi01-ui <zliphadi01@gmail.com>
2025-12-06 16:10:26 +0700

    perbaiki tampilan poli dan kasir

--- config/poli.php
+++ config/poli.php
@@ -1,18 +1,11 @@
 <?php
 
 return [
-    // Simple list of poli/poliklinik options used in forms.
-    // You can later replace this with a DB-driven master table.
+    // Daftar poliklinik rawat jalan
     'options' => [
         'Poli Umum',
-        'Poli Anak',
         'Poli Gigi',
+        'Poli Anak',
         'Poli Kandungan',
-        'Poli Mata',
-        'Poli THT',
-        'Poli Kulit & Kelamin',
-        'Poli Penyakit Dalam',
-        'Poli Bedah',
-        'Poli Kb',
     ],
 ];
