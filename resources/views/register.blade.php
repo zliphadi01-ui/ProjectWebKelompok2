a98440dd3fb6fc458815450b5b500f54b5149b7e
andiimiftahuljannahramlan-ops <andiimiftahuljannahramlan@gmail.com>
2025-12-09 22:44:30 +0700

    ubah nama klinik dibagian register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulir Pendaftaran Pasien Baru - RME Klinik Polije</title>
+    <title>Formulir Pendaftaran Pasien Baru - RME Klinik SIREMIK</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
