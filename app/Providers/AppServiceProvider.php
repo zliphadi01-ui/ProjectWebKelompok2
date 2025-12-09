731c30d673e478a7fc363e26ee9b9aacd0bc71a7
muhammadnaufalmustafa-cpu <muhammadnaufalmustafa@gmail.com>
2025-12-09 16:49:37 +0700

    perbaiki size arrow di master data icd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Pagination\Paginator;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFive();
+        
         // Set Carbon locale to Indonesian
         \Carbon\Carbon::setLocale('id');
         setlocale(LC_TIME, 'id_ID.UTF-8', 'id_ID', 'Indonesian');
